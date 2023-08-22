1c76e57ca4a41f6d2be1d3ff0ee1da4f2d501c10
shinto-10 <21412006si@yamato-univ.jp>
2023-08-22 10:04:41 +0900

    Create build.yml

---
+++ tests/test_CSVPrinter.py
@@ -0,0 +1,20 @@
+import unittest
+from unittest import TestCase
+
+from speciallecture.CSVPrinter import CSVPrinter
+
+
+class TestCSVPrinter(unittest.TestCase):
+    def test_something(self):
+        printer = CSVPrinter("sample.csv")
+        l = printer.read()
+        print(l)
+        self.assertEqual("value2B", l[1][1])  # add assertion here
+
+    def test_read3(self):
+        try:
+            printer = CSVPrinter("sample.csv")
+            printer.read()
+            self.fail("a")
+        except FileNotFoundError as e:
+            pass
